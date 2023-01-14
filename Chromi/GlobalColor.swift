ee9d0604056d7ae3251bb41d9ee2763ebd64b7ab
Will Conrad <jwconrad23@gmail.com>
2023-01-14 11:41:48 -0500

    comment

--- Chromi/GlobalColor.swift
+++ Chromi/GlobalColor.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 class GlobalColor {
+    //Global values + app init values
     static var color = UIColor(hexString: "#5865F2")
     static var inputType = ColorType.rgb
     static var outputType = ColorType.hex

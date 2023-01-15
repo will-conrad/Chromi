159be796516830a5712b7f00c2075c5fe30fb11c
Will Conrad <jwconrad23@gmail.com>
2023-01-15 12:04:28 -0500

    added code to make inputColorView on Swatch VC work

--- Chromi/Controllers/SwatchesViewController.swift
+++ Chromi/Controllers/SwatchesViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class SwatchesViewController: UIViewController {
 
+    
+    @IBOutlet var colorBarContainerView: UIView!
+    @IBOutlet var inputColorStack: UIStackView!
+    
+    @IBOutlet var inputColorLabel: SRCopyableLabel!
+    var colorBarView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let padding: CGFloat = 7
+        
+        colorBarView = UIView(
+           frame: CGRect(
+                   x: padding,
+                   y: padding,
+                   width: colorBarContainerView.frame.width - 2*padding,
+                   height: colorBarContainerView.frame.height - 2*padding))
+       colorBarView.layer.cornerRadius = 10
+       colorBarView.backgroundColor = GlobalColor.color
+       
+       colorBarContainerView.addSubview(colorBarView)
+       inputColorStack.layer.cornerRadius = 10
+       
+       inputColorLabel.text = colorToText(color: GlobalColor.color, type: GlobalColor.inputType)
+       
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        colorBarView.backgroundColor = GlobalColor.color
+        inputColorLabel.text = colorToText(color: GlobalColor.color, type: GlobalColor.inputType)
     }
-    */
-
+    
 }

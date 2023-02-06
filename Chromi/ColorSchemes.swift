b5cfb822efd71069e3229e299da0a6e209c7864e
Will Conrad <jwconrad23@gmail.com>
2023-02-06 17:30:17 -0500

    Removed unused files

--- Chromi/ColorSchemes.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ColorScheme.swift
-//  Chromi
-//
-//  Created by Will Conrad on 1/12/23.
-//
-
-import Foundation
-
--- Chromi/TypeActions.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TypeActions.swift
-//  Chromi
-//
-//  Created by Will Conrad on 1/11/23.
-//
-
-import Foundation
